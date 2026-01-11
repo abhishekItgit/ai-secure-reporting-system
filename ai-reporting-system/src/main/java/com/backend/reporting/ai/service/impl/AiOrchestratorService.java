df8d4dac80c372197df1efa57022aaa54ed3e764
abhishekItgit <mrabhishekdwivediit@gmail.com>
2026-01-11 22:09:27 +0530

    Redis cache

---
+++ ai-reporting-system/src/main/java/com/backend/reporting/ai/service/impl/AiOrchestratorService.java
@@ -0,0 +1,49 @@
+package com.backend.reporting.ai.service.impl;
+
+
+import com.backend.reporting.ai.cache.*;
+import com.backend.reporting.ai.security.SqlSafetyValidator;
+import com.backend.reporting.ai.service.AiSqlService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AiOrchestratorService {
+
+    private final SqlCacheService sqlCacheService;
+    private final AiSqlService aiSqlService;
+    private final SqlSafetyValidator sqlSafetyValidator;
+    private final SchemaHashProvider schemaHashProvider;
+
+    public AiOrchestratorService(SqlCacheService sqlCacheService, AiSqlService aiSqlService, SqlSafetyValidator sqlSafetyValidator, SchemaHashProvider schemaHashProvider) {
+        this.sqlCacheService = sqlCacheService;
+        this.aiSqlService = aiSqlService;
+        this.sqlSafetyValidator = sqlSafetyValidator;
+        this.schemaHashProvider = schemaHashProvider;
+    }
+
+    public String process(String userQuestion) {
+
+        //  Normalize
+        String normalized = QueryNormalizer.normalize(userQuestion);
+
+        // Build cache key
+        String cacheKey = SqlCacheKeyBuilder.build(schemaHashProvider.getSchemaHash(), normalized);
+
+        // Redis HIT → return immediately
+        var cachedSql = sqlCacheService.get(cacheKey);
+        if (cachedSql.isPresent()) {
+            sqlSafetyValidator.validate(cachedSql.get());
+            return cachedSql.get();
+        }
+
+        // 4Redis MISS → call AI
+        String sql = aiSqlService.generateSql(userQuestion, null);
+
+        //  Validate
+        sqlSafetyValidator.validate(sql);
+
+        //Cache & return
+        sqlCacheService.put(cacheKey, sql);
+        return sql;
+    }
+}

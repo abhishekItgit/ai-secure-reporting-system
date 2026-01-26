f7678dfa28558d4563ea4cd183b81c26979a7bf5
abhishekItgit <mrabhishekdwivediit@gmail.com>
2026-01-26 19:18:04 +0530

    vector db  implementation

--- ai-reporting-system/src/main/java/com/backend/reporting/service/impl/ReportExecutionServiceImpl.java
+++ ai-reporting-system/src/main/java/com/backend/reporting/service/impl/ReportExecutionServiceImpl.java
@@ -1,6 +1,7 @@
 package com.backend.reporting.service;
 
 import com.backend.reporting.ai.security.SqlSafetyValidator;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
@@ -13,7 +14,7 @@ public class ReportExecutionServiceImpl implements ReportExecutionService {
     private final SqlSafetyValidator sqlSafetyValidator;
 
     public ReportExecutionServiceImpl(
-            JdbcTemplate jdbcTemplate,
+           @Qualifier("mysqlJdbcTemplate") JdbcTemplate jdbcTemplate,
             SqlSafetyValidator sqlSafetyValidator
     ) {
         this.jdbcTemplate = jdbcTemplate;
